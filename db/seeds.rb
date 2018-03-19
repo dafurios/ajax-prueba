7155d897850cb353a02457e79575fa700f63758b
dafurios <dafurios@gmail.com>
2018-03-19 17:53:34 -0300

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+
+20.times do |i|
+ Enterprise.create(
+  Name: Faker::LeagueOfLegends.quote)
+end
